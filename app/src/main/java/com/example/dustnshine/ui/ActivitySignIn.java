0405b5af5a9f9158304be656e1e2b07c73240998
jcoBon <jerichovon.11@gmail.com>
2022-01-26 14:28:03 +0800

    connected ForgetPassword

--- app/src/main/java/com/example/dustnshine/ui/ActivitySignIn.java
+++ app/src/main/java/com/example/dustnshine/ui/ActivitySignIn.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.dustnshine.MainActivity;
@@ -16,6 +18,11 @@ import com.example.dustnshine.R;
 public class ActivitySignIn extends AppCompatActivity {
 
     private long backButtonCount;
+
+    private AlertDialog.Builder dialogBuilder;
+    private AlertDialog dialog;
+    EditText emailSearch;
+    Button search;
 
     TextView createAcc,forget;
     Button signInBtn;
@@ -29,7 +36,7 @@ public class ActivitySignIn extends AppCompatActivity {
 
         createAcc = findViewById(R.id.createAcc);
         signInBtn = findViewById(R.id.btnServerLogin);
-//        forget = findViewById(R.id.forget);
+        forget = findViewById(R.id.forget);
 
         createAcc.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,13 +54,12 @@ public class ActivitySignIn extends AppCompatActivity {
             }
         });
 
-//        forget.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(ActivitySignIn.this, ActivitySearchEmail.class);
-//                startActivity(intent);
-//            }
-//        });
+        forget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createNewDialog();
+            }
+        });
     }
 
     @Override
@@ -72,5 +78,28 @@ public class ActivitySignIn extends AppCompatActivity {
             Toast.makeText(this, "Press the back button once again to close the application.", Toast.LENGTH_SHORT).show();
             backButtonCount++;
         }
+    }
+
+    public void  createNewDialog(){
+            dialogBuilder = new AlertDialog.Builder(this);
+            final  View searchPopUp = getLayoutInflater().inflate(R.layout.activity_search_email,null);
+            emailSearch = searchPopUp.findViewById(R.id.enterEmailSearch);
+
+            search = searchPopUp.findViewById(R.id.searchBtn);
+
+            dialogBuilder.setView(searchPopUp);
+            dialog = dialogBuilder.create();
+            dialog.show();
+
+            search.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(ActivitySignIn.this, ActivityForgetPassword.class);
+                    startActivity(intent);
+                }
+            });
+
+
+
     }
 }
