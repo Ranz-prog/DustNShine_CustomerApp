226b3dac45fb124d19756a0245f2248852a897ad
thecodeplayer <thecodeplayer23@gmail.com>
2022-01-27 14:13:09 +0700

    Added company details layout

---
+++ app/src/main/java/com/example/dustnshine/ui/ActivityCompanyDetails.java
@@ -0,0 +1,16 @@
+package com.example.dustnshine.ui;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.dustnshine.R;
+
+public class ActivityCompanyDetails extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_company_details);
+    }
+}
